340658cc57457ec032047df317d73e0149e3a135
iOS <iOS@iMac-de-ENG-2.local>
2016-06-02 18:03:04 -0300

    commit inicial

--- Agenda/ViewController.swift
+++ Agenda/ViewController.swift
@@ -10,6 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var datePicker: UIDatePicker!
+
+    @IBOutlet weak var txMensagem: UITextField!
+    
+    @IBAction func btnAgendar(sender: AnyObject) {
+        
+        print("AGENDAR")
+        
+        let localNotification: UILocalNotification = UILocalNotification()
+        localNotification.alertBody = self.txMensagem.text!
+        localNotification.alertTitle = "Lembrete"
+        localNotification.fireDate = self.datePicker.date
+        localNotification.timeZone = self.datePicker.timeZone
+        
+        
+        
+        UIApplication.sharedApplication().scheduleLocalNotification(localNotification)
+        
+        self.txMensagem.resignFirstResponder()
+        self.txMensagem.text = ""
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
